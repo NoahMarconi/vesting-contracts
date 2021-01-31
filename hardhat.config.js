9c6787f5a9126016ffee1c22dd566463146df0bf
JesseAbram <jesseabramowitz@hotmail.com>
2021-01-31 20:14:33 +0100

    deployment task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +16,16 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+task("deploy", "Deploys new token contract")
+.addParam("token", "The Cake token address")
+  .setAction(async (args, hre) => {
+    // We get the contract to deploy
+    const Vesting = await hre.ethers.getContractFactory("VestingWallet");
+    const vesting = await Vesting.deploy(args.token);
+
+    console.log("Vesting deployed to:", vesting.address);
+  });
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -17,6 +33,20 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${MAINNET_PRIVATE_KEY}`],
+    },
+    ganache: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
   solidity: "0.7.3",
 };
 
